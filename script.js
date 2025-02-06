d3f9db3a946323431f5c7d6e2d70971b3a1d1ff0
Oussama Touati <touati.oussama.08@gmail.com>
2025-02-06 20:02:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,75 @@
 // Game Data: Questions, Answers, and Corresponding Messages
-const gameData = []
+const gameData = [
+      {
+        question: "Q1",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q2",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q3",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q4",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q5",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q6",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q7",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q8",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+      {
+        question: "Q9",
+        answers: [
+            { text: "A1", message: "R1" },
+            { text: "A2", message: "R2" },
+            { text: "A3", message: "R3" }
+        ]
+    },
+]
